9cbbb8b7a06cdf50ba44e61109f9bd63d6886ea9
luanMoraes17 <luanlima1107@gmail.com>
2024-10-09 19:33:01 -0300

    JOGO DE ADIVINHAÇÃO

--- jogo_adivinhação.py
+++ jogo_adivinhação.py
@@ -1,16 +1,15 @@
 import random
 
 
-#   GERAR UM NUMERO ALEATORIO ENTRE 1 E 100
 def adivinhacao_de_numero():
     numeros_aleatorios = random.randint(1, 100)
     tentativas = 0
     acertou = False
- #  CRIAR TITULO PARA O APP   
+
     print("Olá , seja bem vindo ao jogo de adivinhação!")
     print("Temos um numero escondido entre 1 a 100. Tente acertar em menor tentativas possiveis!")
 
- #  CRIAR LOOP ATÉ O JOGADOR ADVINHAR O NÚMERO
+
     while not acertou:
         palpite = int(input("Digite um número jogador: "))
         tentativas += 1
@@ -23,5 +22,5 @@ def adivinhacao_de_numero():
             acertou = True
             print(f"Parabéns!!! Você acertou o número {numeros_aleatorios} em {tentativas} tentativas.")
 
- #  CHAMAR A FUNCAO PARA INICIO DO JOGO
+ 
 adivinhacao_de_numero()
