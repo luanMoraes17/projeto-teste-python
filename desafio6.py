02a99f9001d007d61b1b0317000ea6bfdfed8056
luan moraes <luanlima1107@gmail.com>
2024-12-23 00:29:48 -0300

    mudança

--- desafio6.py
+++ desafio6.py
@@ -1,7 +1,7 @@
 # DESAFIO 1 - Crie uma função chamada gerar_nome_completo que recebe como parâmetro o nome e sobrenome de alguém e dá boas vindas para essa pessoa
 def gerar_nome_completo (nome,sobrenome):
     print(f'olá seja bem vindo {nome} {sobrenome}')
-gerar_nome_completo('Luan', 'Moraes')
+gerar_nome_completo('Luan', 'Moraes.')
 
 ## DESAFIO 2 - # Crie uma função chamada calcular_valores que recebe 2 parâmetros
 #  o primeiro o preco de um produto e o segundo parâmetro é a quantidade,
