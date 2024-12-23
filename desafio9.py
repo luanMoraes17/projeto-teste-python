d0a29401082da81ffdb12fc0f29206c934ddbc3e
luan moraes <luanlima1107@gmail.com>
2024-12-22 21:03:01 -0300

    atualizando valor

--- desafio9.py
+++ desafio9.py
@@ -15,7 +15,7 @@ from itertools import zip_longest
 
 # Produto: Produto 5 encontrado no valor de None  
 
-produtos = ['Produto 1', 'Produto 2', 'Produto 3', 'Produto 4', 'Produto 5']
+produtos = ['Produto 1', 'Produto 2', 'Produto 3','Produto 4', 'Produto 5']
 precos = ['R$500,00', 'R$1500,00', 'R$2700,00', 'R$5000,00']
 
 for produto, preco in zip_longest (produtos , precos):
